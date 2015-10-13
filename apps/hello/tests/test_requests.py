aa09cf26a7768d58344385bebf46d677d9afe526
Maks Mospanenko <mmospanenko@gmail.com>
2015-10-13 22:11:34 +0300

    ticket:13 add priority test

--- apps/hello/tests/test_requests.py
+++ apps/hello/tests/test_requests.py
@@ -47,3 +47,13 @@ class RequestsTestCase(TestCase):
         # only latest requests?
         latest_ten = Request.objects.order_by('-time')[:10]
         self.assertEqual(set(latest_ten), set(response.context['requests']))
+
+    def test_by_priority(self):
+        '''
+        Test request by priority
+        '''
+        for i in range(15):
+            Request.objects.create(priority=i)
+        response = self.client.get('/requests/')
+        highest_ten = Request.objects.order_by('-priority')[:10]
+        self.assertEqual(set(highest_ten), set(response.context['requests']))
